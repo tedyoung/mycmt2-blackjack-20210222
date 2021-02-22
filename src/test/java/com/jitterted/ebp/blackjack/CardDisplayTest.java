fa18ecde27404b50d641838c0c11a575ceed549d
Ted M. Young <tedyoung@gmail.com>
2021-02-22 10:44:45 -0800

    Lab 1c: Moved display method to its own class, after refactoring to static method.

--- src/test/java/com/jitterted/ebp/blackjack/CardDisplayTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/CardDisplayTest.java
@@ -9,7 +9,7 @@ class CardDisplayTest {
   public void displayTenAsString() throws Exception {
     Card tenCard = new Card(Suit.HEARTS, Rank.TEN);
 
-    assertThat(tenCard.display())
+    assertThat(ConsoleCard.display(tenCard))
         .isEqualTo("[31m┌─────────┐[1B[11D│10       │[1B[11D│         │[1B[11D│    ♥    │[1B[11D│         │[1B[11D│       10│[1B[11D└─────────┘");
   }
 
@@ -17,7 +17,7 @@ class CardDisplayTest {
   public void displayNonTenAsString() throws Exception {
     Card nonTenCard = new Card(Suit.CLUBS, Rank.QUEEN);
 
-    assertThat(nonTenCard.display())
+    assertThat(ConsoleCard.display(nonTenCard))
         .isEqualTo("[30m┌─────────┐[1B[11D│Q        │[1B[11D│         │[1B[11D│    ♣    │[1B[11D│         │[1B[11D│        Q│[1B[11D└─────────┘");
   }
 }
